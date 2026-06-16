68dffdb7f81e5203ccf21cc643d4564ac65bbd9a
mohamed-044 <mohamed.boulatika04@gmail.com>
2026-06-17 00:01:59 +0200

    deploy storefront with vercel

---
+++ apps/storefront/jest.config.js
@@ -0,0 +1,13 @@
+const nextJest = require("next/jest");
+
+const createJestConfig = nextJest({ dir: "./" });
+
+/** @type {import('jest').Config} */
+const config = {
+  testEnvironment: "jsdom",
+  moduleNameMapper: {
+    "^@/(.*)$": "<rootDir>/src/$1",
+  },
+};
+
+module.exports = createJestConfig(config);
